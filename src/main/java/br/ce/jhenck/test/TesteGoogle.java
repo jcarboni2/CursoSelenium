ae580aa9d9715162483ddbe0d64619d2bbe9f327
Henck <jcarboni2@gmail.com>
2026-09-10 09:31:44 -0300

    Remover arquivos e conteúdo exposto

--- src/main/java/br/ce/jhenck/test/TesteGoogle.java
+++ src/main/java/br/ce/jhenck/test/TesteGoogle.java
@@ -24,8 +24,8 @@ public class TesteGoogle {
 	
 	@Test
 	public void teste() {
-//		System.setProperty("webdriver.gecko.driver", "/Users/jhenck/Downloads/geckodriver");
-//		System.setProperty("webdriver.chrome.driver", "/Users/jhenck/Downloads/chromedriver");
+//		System.setProperty("webdriver.gecko.driver", "/localPath/geckodriver");
+//		System.setProperty("webdriver.chrome.driver", "/localPath/chromedriver");
 //		WebDriver driver = new ChromeDriver();
 //		WebDriver driver = new InternetExplorerDriver();
 		driver.get("http://www.google.com");
